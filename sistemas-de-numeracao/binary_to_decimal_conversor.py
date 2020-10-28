cf077c904a3c07501965d8327ebfc1ae1748e4a5
Jonathas Andrade <Jonathas.andrade5@gmail.com>
2020-10-28 12:41:18 -0300

    first commit

---
+++ sistemas-de-numeracao/binary_to_decimal_conversor.py
@@ -0,0 +1,17 @@
+def revertBinary(binaryString: str):
+    result = 0
+    stringToList = list(binaryString)
+
+    for i in range(len(stringToList)):
+        currentValue = int(stringToList[i])
+        if(currentValue != 1 and currentValue != 0):
+            return "Por favor, o valor deve estar em binário! Tente novamente."
+        else:
+            result += currentValue * \
+                2 ** (len(stringToList) - i - 1)
+
+    return result
+
+
+while True:
+    print(revertBinary(input("Define um valor em binário: ")))
---
+++ sistemas-de-numeracao/decima_to_binary_conversor.py
@@ -0,0 +1,13 @@
+def convertDecimalToBinary(value: str or int):
+    number = int(value)
+    string = ''
+
+    while number > 0:
+        rest = int(number % 2)
+        string += str(rest)
+        number = (number - rest) / 2
+
+    return string[::-1]
+
+
+print(convertDecimalToBinary(input("Informe o valor em decimal: ")))
